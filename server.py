5c703e466ab9c483b91069efd195fa2cc5b0c7a5
Abdelkarim Mouachiq <abmou87@ulaval.ca>
2024-09-28 13:44:49 +0000

    add socket setup

---
+++ server.py
@@ -0,0 +1,9 @@
+import socket
+
+
+def create_server(host='localhost', port=8080):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((host, port))
+    server.listen(5)
+    return server
